60748a9e56c3754fd84f6bc60a35a7ee9459a3f9
Daniel Bergmann <61001914+daniel-bergmann@users.noreply.github.com>
2020-10-25 13:03:57 +0000

    Add files via upload

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <form @submit.prevent="addTodo">
+          <input type="text" v-model="title" name="title" placeholder="Whats up dog?...">
+          <input type="submit" value="submit" class="btn">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false 
+            }
+            // sen up to parent
+            this.$emit('add-todo', newTodo);
+
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    display: flex; 
+    justify-content: center;
+    }
+</style>
